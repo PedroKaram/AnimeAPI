44719566795d79a896ce37666d8cec635aa3543e
Pedro Karam <pedrokaram4005@gmail.com>
2023-03-08 20:12:05 -0300

    First commit

---
+++ AnimeAPI/Models/Anime.cs
@@ -0,0 +1,19 @@
+﻿
+
+namespace AnimeAPI.Models
+{
+    public class Anime
+    {
+        public int Id { get; set; }
+
+        public string TitleJP { get; set; }
+
+        public string TitleEN { get; set; }
+
+        public DateTime RelaseDate { get; set; }
+
+        public bool IsFinished { get; set; }
+
+        public string Description { get; set; }
+    }
+}
